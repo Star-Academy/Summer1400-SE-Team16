fa8c42e53c014e7d4d4511780976fec0814ac707
Shayan <s.salehi1381@gmail.com>
2021-07-30 12:53:20 +0430

    Document Processor Test done

--- Phase01-Search/src/test/java/utils/DocumentProcessorTest.java
+++ Phase01-Search/src/test/java/utils/DocumentProcessorTest.java
@@ -3,6 +3,9 @@ package utils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class DocumentProcessorTest {
     @Test
     void getNormalizedWordsTest() {
@@ -17,5 +20,20 @@ class DocumentProcessorTest {
             Assertions.assertArrayEquals(secondTestExpected, new DocumentProcessor(secondTest).getNormalizedWords());
             Assertions.assertArrayEquals(thirdTestExpected, new DocumentProcessor(thirdTest).getNormalizedWords());
         });
+    }
+
+    @Test
+    void DocumentProcessorConstructorTest() {
+        Set<String> firstTest = new HashSet<>();
+        firstTest.add("t&h*e");
+        firstTest.add("aRt");
+        firstTest.add("O@f");
+        firstTest.add("Creations");
+        firstTest.add("t*he");
+        firstTest.add("jo&b");
+        firstTest.add("foR#");
+        firstTest.add("creators");
+        String[] firstTestExpected = {"job", "art", "the", "of", "the", "creator", "creation", "for"};
+        Assertions.assertArrayEquals(firstTestExpected, new DocumentProcessor(firstTest).getNormalizedWords());
     }
 }
