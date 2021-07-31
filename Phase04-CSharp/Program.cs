281e22c5f7c5a9f9aca1e0af57800a365806f264
AlisaLC <ali_sal1381@yahoo.com>
2021-07-31 13:46:27 +0430

    Phase 4 base commit

---
+++ Phase04-CSharp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (Student Student in GetTopThreeStudents())
+            {
+                PrintStudentData(Student);
+            }
+        }
+
+        private static List<Student> GetTopThreeStudents()
+        {
+            List<Student> Students = DeserializeJsonFile<Student>("Students.json");
+            List<StudentScore> StudentScores = DeserializeJsonFile<StudentScore>("Scores.json");
+            AssignStudentAverages(Students, StudentScores);
+            return Students.OrderByDescending(o => o.Average).Take(3).ToList();
+        }
+
+        private static void AssignStudentAverages(List<Student> Students, List<StudentScore> StudentScores)
+        {
+            foreach (Student Student in Students)
+            {
+                Student.Average = StudentScores.Where(o => o.StudentNumber == Student.StudentNumber).Average(o => o.Score);
+            }
+        }
+
+        private static List<T> DeserializeJsonFile<T>(string FileName) => JsonSerializer.Deserialize<List<T>>(File.ReadAllText(FileName));
+
+        private static void PrintStudentData(Student Student)
+        {
+            Console.WriteLine("{0,-20}{1,-20}{2,-5}", Student.FirstName, Student.LastName, Student.Average.ToString("0.00"));
+        }
+    }
+}
---
+++ Phase04-CSharp/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace First
+{
+    class Student
+    {
+        public int StudentNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [JsonIgnore]
+        public double Average { get; set; }
+    }
+
+    class StudentScore
+    {
+        public int StudentNumber { get; set; }
+        public string Lesson { get; set; }
+        public double Score { get; set; }
+    }
+}
